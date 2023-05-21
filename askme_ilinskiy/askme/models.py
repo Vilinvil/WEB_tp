038155a33d8a71d73dc93f583e5894e8dd8138b5
Vilinvil <ivn15072002@gmail.com>
2023-05-21 20:46:30 +0300

    deleted hardcode in href

--- askme_ilinskiy/askme/models.py
+++ askme_ilinskiy/askme/models.py
@@ -1,14 +1,12 @@
-ASKME_PATH = "/askme/"
-
 QUESTIONS = [
     {
         "title": f"Question {i}",
         "text": f'Text {i} Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн. Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.',
         "answers": f"Answers: {i}",
-        "tags": [{"name": f"tag{i}", "href": f"{ASKME_PATH}tag/{i}"},
-                 {"name": f"tag{i + 1}", "href": f"{ASKME_PATH}tag/{i + 1}"}],
+        "tags": [{"name": f"tag{i}", "id": i},
+                 {"name": f"tag{i + 1}", "id": i + 1}],
         "rating": f"{i}",
-        "href": f"{ASKME_PATH}question/{i}"
+        "id": f"{i}",
     } for i in range(20)
 ]
 
@@ -16,7 +14,7 @@ TAGS = [
     {
        "text": f"Tag{i}",
         "color": f"black",
-        "href": f"{ASKME_PATH}tag/{i}"
+        "id": f"{i}"
     } for i in range(15)
 ]
 
@@ -35,7 +33,7 @@ AUTHOR = {
 
 ANSWERS = [
     {
-        "text": f'Text answer {i}',
+        "text": f'Text answer {i}  Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является стандартной "рыбой" для тек Lorem Ipsum является стандартной "рыбой" для текстов на латини Lorem Ipsum является стандартной "рыбой" для текстов на латини Lorem Ipsum является стандартной "рыбой" для текстов на латини Lorem Ipsum является стандартной "рыбой" для текстов на латинистов на латинице с',
         "rating": f'{i}',
         "isCorrect": False
     }for i in range(8)
